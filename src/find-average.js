a205333bffa14d93e047ecd5f82a6c90c9000972
Maria <naplesaries2014@gmail.com>
2022-06-12 18:09:28 +0200

    displayed average

--- src/find-average.js
+++ src/find-average.js
@@ -7,10 +7,9 @@ import { average } from './utils/average.js';
 const findAverage = () => {
   debugger;
 
-  // calculate the average of all saved numbers  (use your util function!)
-
+  /display('your-output', average(numbers));
   // display the average in the UI
 };
 
 // !! fill in this blank with the correct DOM ID
-document.getElementById('__').addEventListener('click', findAverage);
+document.getElementById('average-them').addEventListener('click', findAverage);
