43d1cbb84cec47df6b745cfe505b5276627633d4
Maria <naplesaries2014@gmail.com>
2022-06-13 14:08:06 +0200

    code for remove-number.js

--- src/remove-number.js
+++ src/remove-number.js
@@ -8,11 +8,13 @@ const removeNumber = () => {
   debugger;
 
   // read the user's number
-
+  const number = readNumber('the-num');
   // remove the user's number from the array
-
+  const removed = numbers.filter((num) => num !== number);
+  numbers.splice(0, numbers.length, ...removed);
   // display the new list of numbers (use the util function!)
+  display('your-output', listItems(numbers));
 };
 
 // !! fill in this blank with the correct DOM ID
-document.getElementById('__').addEventListener('click', removeNumber);
+document.getElementById('remove-it').addEventListener('click', removeNumber);
