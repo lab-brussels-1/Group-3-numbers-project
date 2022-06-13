4405a47b7ce862d99d87b21a16a7951180669dc0
Maria <naplesaries2014@gmail.com>
2022-06-13 14:04:55 +0200

    changed the path

--- src/utils/average.spec.js
+++ src/utils/average.spec.js
@@ -1,4 +1,4 @@
-import { average } from './average.js';
+import { average } from 'average.js';
 
 describe('average: calculates the average of an array of numbers', () => {
   describe('averaging numbers', () => {
