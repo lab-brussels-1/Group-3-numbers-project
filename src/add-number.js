11036b6793a9c79ec1b09b758cd20c0cf8968954
minju <minjupgeorge@gmail.com>
2022-06-16 08:44:55 +0200

    add-number solution

--- src/add-number.js
+++ src/add-number.js
@@ -10,11 +10,13 @@ const addNumber = () => {
   debugger;
 
   // read the user's number
-
+const newNumbers= readNumber('the-num');
   // add it to the array of numbers
-
+numbers.push(newNumbers);
   // generate and display a string listing all numbers
+
+display('your-output', listItems(numbers));
 };
 
 // !! fill in this blank with the correct DOM ID
-document.getElementById('__').addEventListener('click', addNumber);
+document.getElementById('add-it').addEventListener('click', addNumber);
