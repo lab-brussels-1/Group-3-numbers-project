1c5dd4bdef020db9df780024935490895de275d9
minju <minjupgeorge@gmail.com>
2022-06-16 11:49:46 +0200

    sum file modified

--- src/utils/average.js
+++ src/utils/average.js
@@ -1,7 +1,13 @@
 /**
- * Calculates the average of a list of numbers.
- *
- * @param {number[]} [nums=[]] - The numbers to average.
- * @returns {number} The average of all numbers.
- */
-export const average = (nums = []) => {};
+* Calculates the average of a list of numbers.
+*
+* @param {number[]} [nums=[]] - The numbers to average.
+* @returns {number} The average of all numbers.
+*/
+export const average = (nums = []) => {
+ let avg = 0;
+ for (const num of nums) {
+   avg += num / nums.length;
+ }
+ return avg;
+};
--- src/utils/list-items.js
+++ src/utils/list-items.js
@@ -1,23 +1,22 @@
 /**
- * Converts an array if items into a string list.
- *
- * @param {any} [arr=[]] - The items to list.
- * @returns {string} A formatted list of items.
- *
- * @example
- *
- * listItems([1, 2, 3]); //--> "* 1\n* 2\n* 3\n"
- *
- * @example
- *
- * listItems([2, 5, 0, -3]); //--> "* 2\n* 5\n* 0\n* -3\n"
- *
- */
+* Converts an array if items into a string list.
+*
+* @param {any} [arr=[]] - The items to list.
+* @returns {string} A formatted list of items.
+*
+* @example
+*
+* listItems([1, 2, 3]); //--> "* 1\n* 2\n* 3\n"
+*
+* @example
+*
+* listItems([2, 5, 0, -3]); //--> "* 2\n* 5\n* 0\n* -3\n"
+*
+*/
 export const listItems = (arr = []) => {
-debugger;
-let newList = '';
-for (const num of numbers) {
-  newList += '# ${num}\n';
-}
-return newList;
+ let newList = '';
+ for (const num of arr) {
+   newList += `#${num}\n`;
+ }
+ return newList;
 };
