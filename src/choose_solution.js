73bd96a490f1ae10355bc247629755055979a7bd
Maria <naplesaries2014@gmail.com>
2022-06-18 14:56:17 +0200

    deleted teammates function

--- src/choose_solution.js
+++
@@ -1,6 +0,0 @@
-export const teammates = () => {
-  const solution = document.getElementById('team').value;
-  console.log(solution);
-  return solution;
-};
-document.getElementById('all').addEventListener('click', teammates);
